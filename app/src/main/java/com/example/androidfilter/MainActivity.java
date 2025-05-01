5d573673c2dc9c77dac4a978f6bb08462ca371e5
Carl Kaziboni <carlkazibonicman@hotmail.com>
2025-05-01 15:45:38 +0100

    Ability to save photos added

--- app/src/main/java/com/example/androidfilter/MainActivity.java
+++ app/src/main/java/com/example/androidfilter/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.androidfilter;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.Manifest;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -25,18 +30,21 @@ import com.bumptech.glide.request.RequestOptions;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
+import jp.wasabeef.glide.transformations.BlurTransformation;
 import jp.wasabeef.glide.transformations.gpu.SepiaFilterTransformation;
 import jp.wasabeef.glide.transformations.gpu.SketchFilterTransformation;
 import jp.wasabeef.glide.transformations.gpu.ToonFilterTransformation;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     private ImageView imageView;
     private Bitmap image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -50,10 +58,21 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.image_view);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
     public void choosePhoto(View view){
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.setType("image/*");
         startActivityForResult(intent, 1);
+    }
+
+    public void save(View view){
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap imageBitmap = bitmapDrawable.getBitmap();
+        MediaStore.Images.Media.insertImage(getContentResolver(), imageBitmap, "image", "image");
     }
 
     public void applyFilter(Transformation<Bitmap> transformation){
@@ -72,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void applySketch(View view){
         applyFilter(new SketchFilterTransformation());
+    }
+
+    public void applyBlur(View view){
+        applyFilter(new BlurTransformation());
     }
 
     @Override
